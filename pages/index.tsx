0263d0035254afd04195d61916f5bdf449002036
Rohit Joshi <rohit.joshiadvanced@gmail.com>
2022-02-01 16:45:48 +0530

    Add headless-ui - Add navbar - Add header banner with vadhiv graffitti - Add meaning of vadhiv below it

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image"
+import React from "react"
+import Navbar from "./components/Navbar"
+
 const Homepage = () => {
   return (
     <>
-      <h1 className="text-center my-24 font-black tracking-tight text-6xl">Vadhiv</h1>
+      <Navbar/>
+      <div className="relative bg-slate-800">
+        <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
+          
+          <Image src="/vadhiv-watercolor.png" alt="vadhiv-graffiti" height="98" width="336"/>
+          <p className="mt-6 text-xl text-indigo-100 max-w-3xl">
+            A suitable english word would be a whimsical person or an act.
+            Initially it was used to describe a person, by saying stuff such as Kay vadhiv ahe to.
+            But soon enough its other forms started appearing. Vadhivpana, vadhivgiri etc.
+          </p>
+          <p className="mt-1 max-w-2xl text-sm text-gray-400">Example: Arre Rohit kasla vadhiv vagato</p>
+        </div>
+      </div>
+      <div className="relative bg-gray-200">
+        <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
+    
+        </div>
+      </div>
+
     </>
   )
 }
